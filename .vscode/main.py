17ec99037521456951bb925ac9af7e02e83d16b4
AmirMahdi Abooshahab <amir.mahdi.aboo@gmail.com>
2024-02-01 18:26:07 +0330

    outOfScope logic fully implemented

--- .vscode/main.py
+++ .vscode/main.py
@@ -2,6 +2,7 @@ import json
 from difflib import get_close_matches
 from parsivar import Normalizer
 from parsivar import Tokenizer
+from fuzzywuzzy import fuzz
 
 def load_data(file_path: str) -> dict:
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -12,7 +13,7 @@ def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)        
         
-def find_best_match(user_question: str, questions: list) -> str | None:
+def find_best_match_sentence(user_question: str, questions: list) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
 
@@ -32,8 +33,6 @@ def Tokenize_Input(user_input: str):
      
 def classify_input(words_list: list, data: dict) -> tuple | None:
     
-    #data = load_knowledge_base("D:\Daneshgah\Term5\AI\Project\.vscode\Knowledge_base.json")
-    
     for chitChatQuestion in data["ChitChat"]:
         for word in words_list:
             if word == chitChatQuestion["question"]:
@@ -48,8 +47,6 @@ def classify_input(words_list: list, data: dict) -> tuple | None:
         for word in words_list:
             if word == input["input"]:
                 return ("Cart" , word)
-            else: 
-                return ("OutOfScope", word)
     
 def ChooseProperDict(scope : str, loadedData : dict) -> dict :
     if scope == "ChitChat" :
@@ -95,8 +92,34 @@ def ShowCart(user_cart : dict):
         print(f" {details['quantity']} : تعداد")
         print("----------------")
     print( CalculateTotalPrice(user_cart) , " : قیمت کل  ")
-        
-    
+
+
+def find_best_match_sentence(input_sentence, candidate_sentences):
+    best_match_score = 0
+    best_match_sentence = ""
+
+    for candidate in candidate_sentences:
+        similarity_score = fuzz.token_sort_ratio(input_sentence, candidate)
+        if similarity_score > best_match_score:
+            best_match_score = similarity_score
+            best_match_sentence = candidate
+
+    return best_match_sentence, best_match_score        
+
+def get_answer_for_outOfScope(best_match, chosenDict) -> str | None:
+    for q in chosenDict:
+        if q["question"] == best_match:
+            return q["answer"]    
+
+def get_sentence_answer_chitChat(best_match, chosenDict, userInput) -> str | None:
+    chitChat_questions : list = []
+    for OutOfScopeQuestion in chosenDict:
+        chitChat_questions.append(OutOfScopeQuestion["question"])
+            
+    best_match = find_best_match_sentence(userInput, chitChat_questions)
+    for q in chosenDict:
+        if q["question"] == best_match[0]:
+            return q["answer"]
 
 def chat_bot():
     loaded_data: dict = load_data("D:\Daneshgah\Term5\AI\Project\.vscode\Knowledge_base.json")
@@ -109,38 +132,50 @@ def chat_bot():
         
         input_words_list = Tokenize_Input(raw_user_input)
         chosenTuple = classify_input(input_words_list,loaded_data)
-        print(chosenTuple)
-        if(chosenTuple == None):
-            print(Normalizer().normalize("بات : متوجه نشدم"))
-            continue
-        founded_word = chosenTuple[1]
-        chosen_scope = chosenTuple[0]
-        chosenDict = ChooseProperDict(chosen_scope, loaded_data)
-        if chosen_scope == "ChitChat":
-            answer : str = get_answer_for_chitChat(founded_word, chosenDict)
-            print(Normalizer().normalize(answer))
-            
-        elif chosen_scope == "Inventory":
-            product_data : tuple = get_answer_for_inventory(founded_word, chosenDict)
-            product_quantity : int = product_data[0]
-            product_price : int = product_data[1]
-            if product_quantity > 0:
-                print("{} تعداد این کالا ".format(product_quantity))
-                print("{}  قیمت هر عدد آن ".format(product_price))
-                print(Normalizer().normalize("تعداد کالا را وارد کنید :"))
-                user_required_quantity : int = int(input())
-                if(user_required_quantity > product_quantity):
-                    print(Normalizer().normalize("درخواست شما بیشتر از موجودی است"))
-                    continue
-                UpdateLoadedData(loaded_data, founded_word, user_required_quantity)
-                UpdateUserCart(user_cart, founded_word, user_required_quantity, product_quantity, product_price)
-                print(Normalizer().normalize("کالا به سبد خرید اضافه شد"))
-                print (Normalizer().normalize("آیا مورد دیگری نیاز دارید؟"))
-            else:
-                print(Normalizer().normalize("موجودی کالا تمام شده است"))
+        if(chosenTuple != None):
+            founded_word = chosenTuple[1]
+            chosen_scope = chosenTuple[0]
+            chosenDict = ChooseProperDict(chosen_scope, loaded_data)
+            if chosen_scope == "ChitChat":
+                answer : str = get_answer_for_chitChat(founded_word, chosenDict)
+                #answer : str = get_sentence_answer_chitChat(founded_word, chosenDict, raw_user_input)
+                print(answer)
                 
-        elif chosen_scope == "Cart" :
-            ShowCart(user_cart)
+            elif chosen_scope == "Inventory":
+                product_data : tuple = get_answer_for_inventory(founded_word, chosenDict)
+                product_quantity : int = product_data[0]
+                product_price : int = product_data[1]
+                if product_quantity > 0:
+                    print("{} تعداد این کالا ".format(product_quantity))
+                    print("{}  قیمت هر عدد آن ".format(product_price))
+                    print(Normalizer().normalize("تعداد کالا را وارد کنید :"))
+                    user_required_quantity : int = int(input())
+                    if(user_required_quantity > product_quantity):
+                        print(Normalizer().normalize("درخواست شما بیشتر از موجودی است"))
+                        continue
+                    UpdateLoadedData(loaded_data, founded_word, user_required_quantity)
+                    UpdateUserCart(user_cart, founded_word, user_required_quantity, product_quantity, product_price)
+                    print(Normalizer().normalize("کالا به سبد خرید اضافه شد"))
+                    print (Normalizer().normalize("آیا مورد دیگری نیاز دارید؟"))
+                else:
+                    print(Normalizer().normalize("موجودی کالا تمام شده است"))
+                    
+            elif chosen_scope == "Cart" :
+                ShowCart(user_cart)
+        else:
+            chosen_scope = "OutOfScope"
+            chosenDict = loaded_data["OutOfScope"]
+            outOfScope_questions : list = []
+            for OutOfScopeQuestion in chosenDict:
+                outOfScope_questions.append(OutOfScopeQuestion["question"])
+           
+            best_match = find_best_match_sentence(raw_user_input, outOfScope_questions)
+            if(best_match[0] == "" or best_match[0] == None or best_match[1] < 60):
+                print(Normalizer().normalize("بات : متوجه نشدم"))
+                continue
+            else:
+                answer = get_answer_for_outOfScope(best_match[0], chosenDict)
+                print(answer)
                 
             
         
@@ -151,11 +186,11 @@ def chat_bot():
             #print(Normalizer().normalize("پاسخی یافت نشد"))
            #new_answer: str = input("Type the answer or 'skip' to skip : ")
             
-        '''if new_answer.lower() != "skip":
-                knowledge_base["ChitChat"].append({"question":user_input,"answer":new_answer})
-                save_knowledge_base("knowledge_base.json",knowledge_base)
-                #print("Bot: Thanks for teaching me a new thing")
-                print(Normalizer().normalize("ممنون که چیز جدیدی یادم دادی"))'''
+        # if new_answer.lower() != "skip":
+        #         knowledge_base["ChitChat"].append({"question":user_input,"answer":new_answer})
+        #         save_knowledge_base("knowledge_base.json",knowledge_base)
+        #         #print("Bot: Thanks for teaching me a new thing")
+        #         print(Normalizer().normalize("ممنون که چیز جدیدی یادم دادی"))
         
 if __name__ == "__main__":
     chat_bot()
